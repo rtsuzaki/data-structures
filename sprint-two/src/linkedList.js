6f97bdd5d0919838c661eb9a04ae5eea73f9b12d
rtsuzaki <rtsuzaki10@gmail.com>
2018-04-26 16:09:08 -0700

    completed addToTail

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,23 +2,42 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  // list.1 = {value: 12, next: list.2}
+  // list.2 = {value: 12, null}
+
+  list.count = 0; //3
 
   list.addToTail = function(value) {
+    if (list.head === null) {
+      var newNode = Node(value)
+      list.count = newNode 
+      list.head = list.count;
+      list.tail = list.count;
+      list.count++;
+    } else {
+      var newNode = Node(value);
+      list.count = newNode;
+      list.count-1.next = list.count;
+      list.tail = list.count;
+      list.count++;
+      }
   };
 
   list.removeHead = function() {
+//list.count--;
   };
 
   list.contains = function(target) {
   };
 
   return list;
+    }
 };
 
-var Node = function(value) {
+var Node = function(val) {
   var node = {};
 
-  node.value = value;
+  node.value = val;
   node.next = null;
 
   return node;
