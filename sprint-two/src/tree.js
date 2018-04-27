2e112e20cde8e93788c865a58fd5d835aaa65e9e
rtsuzaki <rtsuzaki10@gmail.com>
2018-04-27 11:10:45 -0700

    completed addChild for tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,15 +1,19 @@
 var Tree = function(value) {
+    debugger;
   var newTree = {};
   extend(newTree, treeMethods);
   newTree.value = value;
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me (it was null)
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+// newTree.children.push(  )
+  var newNode = Tree(value);
+  this.children.push(newNode);  
 };
 
 treeMethods.contains = function(target) {
@@ -21,7 +25,11 @@ var extend = function (obj1, obj2) {
   }
 }
 
-
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+// var testTree = Tree(4);
+
+// testTree.addChild(5);
+// testTree.children[0].addChild(5)
