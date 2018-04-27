72fae0fccebc29fa13b48873e3ffdae23b513446
rtsuzaki <rtsuzaki10@gmail.com>
2018-04-26 21:35:59 -0700

    completed linkedList and passed all tests

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,47 +2,64 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
-  // list.1 = {value: 12, next: list.2}
-  // list.2 = {value: 12, null}
-
-  list.count = 0; //3
+  var count = 0;
+  var countString = count.toString();
+  var beforeCountString = (count-1).toString();
+  // list.previous = count
 
   list.addToTail = function(value) {
     if (list.head === null) {
-      var newNode = Node(value)
-      list.count = newNode 
-      list.head = list.count;
-      list.tail = list.count;
-      list.count++;
+      var newNode = Node(value);
+      list[countString] = newNode;
+      list.head = list[countString];
+      list.tail = list[countString];
+      count++;
+      countString = count.toString();
+      beforeCountString = (count-1).toString();
     } else {
       var newNode = Node(value);
-      list.count = newNode;
-      list.count-1.next = list.count;
-      list.tail = list.count;
-      list.count++;
-      }
+      list[countString] = newNode;
+      list[beforeCountString].next = list[countString];
+      list[countString] = newNode;
+      list.tail = list[countString];
+      count++;
+      countString = count.toString();
+      beforeCountString = (count-1).toString();
+    }
   };
 
   list.removeHead = function() {
-//list.count--;
+    var currentHead = list.head;
+    var temp = list.head.next;
+    var tempHeadValue = currentHead.value; 
+    list.head.next = null;
+    list.head.value = null;
+    list.head = temp;
+    return tempHeadValue;
+    //returns former head node
   };
 
   list.contains = function(target) {
+    var result = false;
+    for (var key in list) {
+      if (list[key].value === target) {
+        result = true;
+      }
+      // if (key.next === null) {
+      //   return false;
+      // }
+    }
+    return result;
   };
 
   return list;
-    }
 };
 
-var Node = function(val) {
+var Node = function(value) {
   var node = {};
 
-  node.value = val;
+  node.value = value;
   node.next = null;
 
   return node;
 };
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
