2cb05be59d80ad3615cd4e50c393691b71c4a5f6
rtsuzaki <rtsuzaki10@gmail.com>
2018-04-27 18:49:43 -0700

    completed tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,5 +1,4 @@
 var Tree = function(value) {
-    debugger;
   var newTree = {};
   extend(newTree, treeMethods);
   newTree.value = value;
@@ -17,13 +16,27 @@ treeMethods.addChild = function(value) {
 };
 
 treeMethods.contains = function(target) {
+  var isFound = false;
+  var nodeSearch = function(node) {
+    if (node.value === target) {
+      isFound = true;
+    } else { //check children array
+      for (var i = 0; i < node.children.length; i++) {
+        nodeSearch(node.children[i]);
+      }
+    } 
+  }
+  nodeSearch(this);   
+  return isFound;
 };
 
-var extend = function (obj1, obj2) {
+
+var extend = function(obj1,obj2) {
   for (var key in obj2) {
     obj1[key] = obj2[key];
   }
 }
+
 
 /*
  * Complexity: What is the time complexity of the above functions?
